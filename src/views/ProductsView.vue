d2ce86732b04504e5e5a4107fb8817654b2aad5d
Anton Lehrberg <lehrberg.anton@gmail.com>
2024-05-13 19:40:49 +0200

    Put productsFilters inside productsView instead of comp.

--- src/views/ProductsView.vue
+++ src/views/ProductsView.vue
@@ -15,7 +15,158 @@
 
     <section class="products">
       <div class="section__wrapper">
-        <ProductsFiltersComp />
+        <div class="products__filters">
+          <h2 class="h3-size">FILTERS</h2>
+
+          <ul class="filters__list">
+            <li>
+              <Dropdown2Comp header="CATEGORIES">
+                <div class="dropdown__options-wrapper">
+                  <div class="dropdown__option-box">
+                    <input
+                      type="checkbox"
+                      name="categories-slippers"
+                      id="categories-slippers"
+                    />
+                    <label for="categories-slippers">Slippers</label>
+                  </div>
+                  <div class="dropdown__option-box">
+                    <input
+                      type="checkbox"
+                      name="categories-dress-shoes"
+                      id="categories-dress-shoes"
+                    />
+                    <label for="categories-dress-shoes">Dress shoes</label>
+                  </div>
+                  <div class="dropdown__option-box">
+                    <input
+                      type="checkbox"
+                      name="categories-sneakers"
+                      id="categories-sneakers"
+                    />
+                    <label for="categories-sneakers">Sneakers</label>
+                  </div>
+                </div>
+              </Dropdown2Comp>
+            </li>
+            <li>
+              <Dropdown2Comp header="BRAND">
+                <div class="dropdown__options-wrapper">
+                  <div class="dropdown__option-box">
+                    <input
+                      type="checkbox"
+                      name="brand-sneakers"
+                      id="brand-sneakers"
+                    />
+                    <label for="brand-sneakers">Sneakers</label>
+                  </div>
+                  <div class="dropdown__option-box">
+                    <input
+                      type="checkbox"
+                      name="brand-stepside"
+                      id="brand-stepside"
+                    />
+                    <label for="brand-stepside">Stepside</label>
+                  </div>
+                  <div class="dropdown__option-box">
+                    <input
+                      type="checkbox"
+                      name="brand-lejon"
+                      id="brand-lejon"
+                    />
+                    <label for="brand-lejon">Lejon</label>
+                  </div>
+                </div>
+              </Dropdown2Comp>
+            </li>
+            <li>
+              <Dropdown2Comp header="PRICE" parentheses="€">
+                <div class="dropdown__options-wrapper">
+                  <SliderDoubleComp :minValue="50" :maxValue="345" />
+                </div>
+              </Dropdown2Comp>
+            </li>
+            <li>
+              <Dropdown2Comp header="COLOR">
+                <div class="dropdown__options-wrapper">
+                  <div class="dropdown__option-color">
+                    <input type="checkbox" name="color-red" id="color-red" />
+                    <label
+                      for="color-red"
+                      style="background-color: red"
+                    ></label>
+                  </div>
+                  <div class="dropdown__option-color">
+                    <input
+                      type="checkbox"
+                      name="color-yellow"
+                      id="color-yellow"
+                    />
+                    <label
+                      for="color-yellow"
+                      style="background-color: yellow"
+                    ></label>
+                  </div>
+                  <div class="dropdown__option-color">
+                    <input
+                      type="checkbox"
+                      name="color-black"
+                      id="color-black"
+                    />
+                    <label
+                      for="color-black"
+                      style="background-color: black"
+                    ></label>
+                  </div>
+                </div>
+              </Dropdown2Comp>
+            </li>
+            <li>
+              <Dropdown2Comp header="SIZE" parentheses="EU">
+                <div class="dropdown__options-wrapper">
+                  <div class="dropdown__option-box">
+                    <input type="checkbox" name="size-42" id="size-42" />
+                    <label for="size-42">42</label>
+                  </div>
+                </div>
+              </Dropdown2Comp>
+            </li>
+            <li>
+              <Dropdown2Comp header="MATERIAL">
+                <div class="dropdown__options-wrapper">
+                  <div class="dropdown__option-box">
+                    <input
+                      type="checkbox"
+                      name="material-syntet"
+                      id="material-syntet"
+                    />
+                    <label for="material-syntet">Syntet</label>
+                  </div>
+                </div>
+              </Dropdown2Comp>
+            </li>
+            <li>
+              <Dropdown2Comp header="ON SALE">
+                <div class="dropdown__options-wrapper">
+                  <div class="dropdown__option-radio-1__wrapper">
+                    <label class="dropdown__option-radio-1">
+                      Show all
+                      <input type="radio" name="on-sale" checked />
+                    </label>
+                    <label class="dropdown__option-radio-1">
+                      No sale
+                      <input type="radio" name="on-sale" />
+                    </label>
+                    <label class="dropdown__option-radio-1">
+                      On sale
+                      <input type="radio" name="on-sale" />
+                    </label>
+                  </div>
+                </div>
+              </Dropdown2Comp>
+            </li>
+          </ul>
+        </div>
 
         <ProductsGalleryComp :shoes="shoes" />
       </div>
@@ -26,18 +177,20 @@
 <script lang="ts">
 import { defineComponent } from "vue";
 
-import ProductsNavComp from "../components/ProductsNavComp.vue";
-import ProductsFiltersComp from "@/components/ProductsFiltersComp.vue";
+import ProductsNavComp from "@/components/ProductsNavComp.vue";
 import ProductsGalleryComp from "@/components/ProductsGalleryComp.vue";
+import Dropdown2Comp from "@/components/Dropdown2Comp.vue";
+import SliderDoubleComp from "@/components/SliderDoubleComp.vue";
 
 import { getProductsMale, getProductsFemale } from "@/services/products";
 
 export default defineComponent({
-  name: "MenShoesView",
+  name: "ProductsView",
   components: {
     ProductsNavComp,
-    ProductsFiltersComp,
     ProductsGalleryComp,
+    Dropdown2Comp,
+    SliderDoubleComp,
   },
 
   data() {
@@ -91,5 +244,20 @@ export default defineComponent({
   flex-direction: row;
   align-items: start;
   gap: 4rem;
+}
+
+/* FILTERS */
+.products__filters {
+  width: 15rem;
+
+  display: flex;
+  flex-direction: column;
+  gap: 2.5rem;
+}
+
+.filters__list {
+  display: flex;
+  flex-direction: column;
+  gap: 2.5rem;
 }
 </style>
