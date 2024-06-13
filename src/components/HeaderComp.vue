a761b0911a50e14a7b40f70edfe7ceb301eb1244
Anton Lehrberg <lehrberg.anton@gmail.com>
2024-06-13 14:40:03 +0200

    Make header responsive with hamburger menu on smaller screens.

--- src/components/HeaderComp.vue
+++ src/components/HeaderComp.vue
@@ -12,7 +12,7 @@
           /></router-link>
         </li>
 
-        <li>
+        <li class="header--large">
           <div class="dropdown-1">
             <router-link class="btn" to="/products/men">
               <span>Men</span>
@@ -32,7 +32,7 @@
             </ul>
           </div>
         </li>
-        <li>
+        <li class="header--large">
           <div class="dropdown-1">
             <router-link class="btn" to="/products/women">
               <span>Women</span>
@@ -55,10 +55,10 @@
             </ul>
           </div>
         </li>
-        <li>
+        <li class="header--large">
           <router-link to="/about">About</router-link>
         </li>
-        <li>
+        <li class="header--large">
           <router-link to="/contact">Contact</router-link>
         </li>
       </ul>
@@ -95,6 +95,70 @@
             <AccountCircleIcon width="32" height="32"
           /></router-link>
         </li>
+        <li class="header--medium">
+          <div class="hamburger-menu" ref="hamburgerMenu">
+            <button @click="menuOpen = !menuOpen" class="btn--link">
+              <HamburgerMenuIcon :size="'2rem'" :is-cross="menuOpen" />
+            </button>
+
+            <nav v-if="menuOpen">
+              <ul>
+                <li>
+                  <div class="dropdown-1">
+                    <router-link class="btn" to="/products/men">
+                      <span>Men</span>
+                      <ExpandMoreIcon
+                    /></router-link>
+
+                    <ul class="dropdown__content">
+                      <li
+                        v-for="maleCategory in getMaleCategories"
+                        :key="maleCategory"
+                      >
+                        <router-link
+                          :to="{
+                            name: 'productsMen',
+                            query: { categories: maleCategory },
+                          }"
+                          >{{ maleCategory }}</router-link
+                        >
+                      </li>
+                    </ul>
+                  </div>
+                </li>
+                <li>
+                  <div class="dropdown-1">
+                    <router-link class="btn" to="/products/women">
+                      <span>Women</span>
+                      <ExpandMoreIcon
+                    /></router-link>
+
+                    <ul class="dropdown__content">
+                      <li
+                        v-for="femaleCategory in getFemaleCategories"
+                        :key="femaleCategory"
+                      >
+                        <router-link
+                          :to="{
+                            name: 'productsWomen',
+                            query: { categories: femaleCategory },
+                          }"
+                          >{{ femaleCategory }}</router-link
+                        >
+                      </li>
+                    </ul>
+                  </div>
+                </li>
+                <li>
+                  <router-link to="/about">About</router-link>
+                </li>
+                <li>
+                  <router-link to="/contact">Contact</router-link>
+                </li>
+              </ul>
+            </nav>
+          </div>
+        </li>
       </ul>
     </nav>
   </header>
@@ -108,6 +172,8 @@ import ExpandMoreIcon from "@/components/icons/ExpandMoreIcon.vue";
 import FavoriteIcon from "@/components/icons/FavoriteIcon.vue";
 import ShoppingBagIcon from "@/components/icons/ShoppingBagIcon.vue";
 import AccountCircleIcon from "@/components/icons/AccountCircleIcon.vue";
+import HamburgerMenuIcon from "@/components/icons/HamburgerMenuIcon.vue";
+
 import store from "@/store";
 
 export default defineComponent({
@@ -117,6 +183,19 @@ export default defineComponent({
     FavoriteIcon,
     ShoppingBagIcon,
     AccountCircleIcon,
+    HamburgerMenuIcon,
+  },
+
+  data() {
+    return {
+      menuOpen: false,
+    };
+  },
+
+  watch: {
+    $route() {
+      this.menuOpen = false;
+    },
   },
 
   computed: {
@@ -165,5 +244,70 @@ header {
 header button,
 header a {
   text-transform: uppercase;
+}
+
+/* ____ HAMBURGER MENU NAV ___ */
+.hamburger-menu {
+  position: relative;
+}
+.hamburger-menu > button {
+  display: block;
+}
+
+.hamburger-menu > nav {
+  z-index: 3;
+  position: absolute;
+  top: calc(100% + 0.6em);
+  right: 0;
+
+  padding: 1.6em 1.8em 2em;
+
+  --color-background: var(--black);
+  background-color: var(--white);
+  border-radius: 4px;
+  border: 1px solid var(--color-grey--light-2);
+  --color-text: var(--black);
+
+  transform-origin: calc(100% - 0.2em) top;
+  animation: hamburger-menu-open 0.2s forwards ease-in-out;
+}
+.hamburger-menu > nav > ul {
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  gap: 0.5em;
+}
+.hamburger-menu > nav > ul ul {
+  --color-text: var(--white-2);
+}
+
+@keyframes hamburger-menu-open {
+  0% {
+    opacity: 0;
+    transform: scaleX(0.1) scaleY(0) scaleZ(1);
+  }
+  20% {
+    opacity: 0.1;
+    transform: scaleX(0.2) scaleY(0.4) scaleZ(1);
+  }
+  100% {
+    opacity: 1;
+    transform: scaleX(1) scaleY(1) scaleZ(1);
+  }
+}
+
+.header--large {
+  display: block;
+}
+.header--medium {
+  display: none;
+}
+@media only screen and (max-width: 50rem) {
+  .header--large {
+    display: none;
+  }
+  .header--medium {
+    display: block;
+  }
 }
 </style>
---
+++ src/components/icons/HamburgerMenuIcon.vue
@@ -0,0 +1,84 @@
+<template>
+  <svg
+    :width="size"
+    :height="size"
+    :class="{ 'is-cross': isCross }"
+    viewBox="0 0 26 26"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      class="line-center"
+      d="M4.0625 13H21.9375"
+      :stroke="color"
+      stroke-width="2"
+      stroke-linecap="square"
+      stroke-linejoin="round"
+    />
+    <path
+      class="line-top"
+      d="M4.0625 6.5H21.9375"
+      :stroke="color"
+      stroke-width="2"
+      stroke-linecap="square"
+      stroke-linejoin="round"
+    />
+    <path
+      class="line-bottom"
+      d="M4.0625 19.5H21.9375"
+      :stroke="color"
+      stroke-width="2"
+      stroke-linecap="square"
+      stroke-linejoin="round"
+    />
+  </svg>
+</template>
+
+<script>
+export default {
+  props: {
+    size: {
+      type: String,
+      default: "1.625rem",
+    },
+
+    isCross: {
+      type: Boolean,
+      default: false,
+    },
+
+    color: {
+      type: String,
+      default: "currentColor",
+    },
+  },
+};
+</script>
+
+<style scoped>
+.line-top,
+.line-center,
+.line-bottom {
+  transform: translateY(0) translateX(0%) scaleX(100%) rotateZ(0deg);
+  transform-origin: center;
+
+  opacity: 1;
+
+  transition: transform 0.1s ease-in-out, opacity 0.1s ease-in-out;
+}
+
+.is-cross .line-top,
+.is-cross .line-center,
+.is-cross .line-bottom {
+  transition: transform 0.2s ease-in-out, opacity 0.1s ease-in-out;
+}
+.is-cross .line-top {
+  transform: translateY(18%) translateX(-18%) rotateZ(45deg) scaleX(120%);
+}
+.is-cross .line-center {
+  opacity: 0;
+}
+.is-cross .line-bottom {
+  transform: translateY(-18%) translateX(-18%) rotateZ(-45deg) scaleX(120%);
+}
+</style>
