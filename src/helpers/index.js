8db43b52d9439019af0055bb993a3e2836b08f4b
Anton Lehrberg <lehrberg.anton@gmail.com>
2024-06-25 21:48:55 +0200

    Remove unused helper function.

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -8,22 +8,6 @@ export function getLocalStorageCart() {
 export function setLocalStorageCart(_cart) {
   localStorage.setItem("cart", JSON.stringify(_cart));
 }
-export function removeProductLocalStorageCart(_productId) {
-  const localStorageCart = getLocalStorageCart();
-  console.log(localStorageCart);
-
-  // Find the index of the object with the given id
-  const index = localStorageCart.findIndex((item) => item.id === _productId);
-  if (index === -1) {
-    return;
-  }
-
-  // Remove product from cart
-  localStorageCart.splice(index, 1);
-
-  setLocalStorageCart(localStorageCart);
-  return localStorageCart;
-}
 export function clearLocalStorageCart() {
   localStorage.removeItem("cart");
 }
